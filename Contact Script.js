feb053ad490d8312eee9cc13f6b2cef8630c053a
Vikas <vikas.kumar.singh1198@gmail.com>
2025-02-10 23:54:48 +0530

    Fixed Form Validation Issue

--- Contact Script.js
+++ Contact Script.js
@@ -5,11 +5,35 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     let email = document.getElementById("email").value.trim();
     let message = document.getElementById("message").value.trim();
 
-    if (!name || !email || !message) {
-        alert("Please fill in all required fields.");
+    const errors = validateForm(name,email,message);
+
+    if(Object.keys(errors).length>0){
+        if (errors.name) document.getElementById("name-error").textContent = errors.name;
+        if (errors.email) document.getElementById("email-error").textContent = errors.email;
+        if (errors.message) document.getElementById("message-error").textContent = errors.message;
         return;
     }
 
     document.getElementById("successMessage").classList.remove("hidden");
     this.reset();
 });
+
+function validateForm(name,email,message){
+    const error = {}
+    const nameValue = /^[A-Za-z\s]+$/.test(name)
+    const emailValue = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const messageValue = /^.{1,500}$/.test(message);
+
+    if(!nameValue){
+        error.name = "Enter a valid name (only letters & spaces allowed)";
+    }
+    if(!emailValue){
+        error.email =  "Enter a valid email address";
+    }
+
+    if(!messageValue){
+        error.message =  "Message must be between 1 and 500 characters";
+
+    return error;
+}
+}
